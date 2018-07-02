a85b6cbe17541a233791ead1ac57fb6c6368e478
Amir <amir@amirs-mac.local>
2018-07-02 01:43:22 -0700

    Initial Commit

---
+++ appGet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appGet
+//
+//  Created by Amir on 7/2/18.
+//  Copyright © 2018 uni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
